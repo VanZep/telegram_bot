d0b5ce74fc9f7613fde625a0067ee8734bc94e3f
Иван Озеров <vanzup@mail.ru>
2023-04-30 15:54:45 +0300

    first try

--- homework.py
+++ homework.py
@@ -2,6 +2,12 @@ import os
 
 import requests
 
+import time
+
+import logging
+
+from telegram import Bot
+
 from dotenv import load_dotenv
 
 
@@ -23,48 +29,125 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    filename='homework.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s, %(levelname)s, %(funcName)s, %(message)s'
+)
+
 
 def check_tokens():
-    ...
+    """Функция проверяет доступность переменных окружения."""
+    if not bool(PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID):
+        logging.critical('Required environment variables are missing')
+        SystemExit()
 
 
 def send_message(bot, message):
-    ...
+    """Функция отправляет сообщение в Telegram чат."""
+    try:
+        bot.send_message(
+            chat_id=TELEGRAM_CHAT_ID,
+            text=message
+        )
+        logging.debug('Bot sent message to Telegram')
+    except Exception as error:
+        logging.error(f'Error "{error}" when sending a message to Telegram')
 
 
 def get_api_answer(timestamp):
-    ...
+    """Функция делает запрос к единственному эндпоинту API-сервиса."""
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=timestamp
+        )
+    except requests.RequestException as error:
+        message = f'Error when requesting the API: {error}'
+        logging.error(message)
+
+    if response.status_code != 200:
+        message = (
+            f'Endpoint {ENDPOINT} unavailable. '
+            f'Status code: {response.status_code}'
+        )
+        logging.error(message)
+        raise AssertionError(message)
+
+    return response.json()
 
 
 def check_response(response):
-    ...
+    """Функция проверяет ответ API на соответствие документации."""
+    if not isinstance(response, dict):
+        message = 'The data must be of the dict type'
+        logging.error(message)
+        raise TypeError(message)
+    if not isinstance(response.get('homeworks'), list):
+        message = 'The data must be of the list type'
+        logging.error(message)
+        raise TypeError(message)
+    if response.get('homeworks') is None:
+        message = 'The "homework" key not found'
+        logging.error(message)
+        raise KeyError(message)
+    if response.get('current_date') is None:
+        message = 'The "current_date" key not found'
+        logging.error(message)
+        raise KeyError(message)
+
+    return response.get('homeworks')
 
 
 def parse_status(homework):
-    ...
+    """Функция извлекает из информации о конкретной домашней работе
+    статус этой работы.
+    """
+    if 'homework_name' not in homework:
+        message = 'The name of the homework is missing'
+        logging.error(message)
+        raise ValueError(message)
+    if homework.get('status') not in HOMEWORK_VERDICTS:
+        message = 'Status unknown or missing'
+        logging.error(message)
+        raise ValueError(message)
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    homework_name = homework.get('homework_name')
+    verdict = HOMEWORK_VERDICTS[homework.get('status')]
+
+    return (
+        'Изменился статус проверки работы '
+        f'"{homework_name}". {verdict}'
+    )
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
-
-    ...
+    check_tokens()
+    bot = Bot(token=TELEGRAM_TOKEN)
+    timestamp = {'from_date': int(time.time())}
 
     while True:
         try:
+            response = get_api_answer(timestamp)
+            homework = check_response(response)
+            if homework:
+                message = parse_status(homework[0])
+                send_message(bot, message)
+            else:
+                logging.debug('Status not updated')
 
-            ...
+            timestamp = {'from_date': response.get('current_date')}
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            msg = f'Сбой в работе программы: {error}'
+            logging.error(msg)
+            send_message(bot, msg)
+
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
